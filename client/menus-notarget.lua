83132f40acb98b1911ba9dc1d14ff8b8dca32e13
Zach <67765614+DrBlackBeard095@users.noreply.github.com>
2021-09-10 08:42:00 +0200

    Still needs testing
    If u want u can use it so long

---
+++ client/menus-notarget.lua
@@ -0,0 +1,366 @@
+
+-- Drawtext -
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        if isLoggedIn then
+            if PlayerJob.name == "burgershot" then
+                local pos = GetEntityCoords(PlayerPedId())
+
+                for k, v in pairs(Config.Locations["duty"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 5 then
+                        if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                            if not onDuty then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ - Get on duty")
+                            else
+                                DrawText3D(v.x, v.y, v.z, "~r~E~w~ - Get off duty")
+                            end
+                            if IsControlJustReleased(0, 38) then
+                                onDuty = not onDuty
+                                TriggerServerEvent("QBCore:ToggleDuty")
+                            end
+                        elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                            DrawText3D(v.x, v.y, v.z, "on/off duty")
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["pattycooker"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ - Cook Patties")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("qb-burgershot:PattyFry")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Cook Patties")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["fryer"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ - Fry Sum Fries")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("qb-burgershot:Fries")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Fry Sum Fries")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["drinks"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Make Drinks")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("nh-context:DrinkMenu")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Make Drinks")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["fridge"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Open Fridge")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("nh-context:OrderMenu")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Open Fridge")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["storage"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Open Storage")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("qb-burgershot:Storage")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Open Storage")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["craftburger"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Make a Meal")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("nh-context:Burgers")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Make a Meal")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["cashregister"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Cash Register")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("qb-burgershot:bill")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Cash Register")
+                            end  
+                        end
+                    end
+                end
+
+                for k, v in pairs(Config.Locations["garage"]) do
+                    if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                        if onDuty then
+                            if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                                DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Garage")
+                                if IsControlJustReleased(0, 38) then
+                                    TriggerEvent("garage:BurgerShotGarage")
+                                end
+                            elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                                DrawText3D(v.x, v.y, v.z, "Garage")
+                            end  
+                        end
+                    end
+                end
+            else
+                Citizen.Wait(2000)
+            end
+            
+            for k, v in pairs(Config.Locations["tray1"]) do
+                if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                    if onDuty then
+                        if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                            DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Tray")
+                            if IsControlJustReleased(0, 38) then
+                                TriggerEvent("qb-burgershot:Tray1")
+                            end
+                        elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                            DrawText3D(v.x, v.y, v.z, "Tray")
+                        end  
+                    end
+                end
+            end
+
+            for k, v in pairs(Config.Locations["tray2"]) do
+                if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                    if onDuty then
+                        if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                            DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Tray")
+                            if IsControlJustReleased(0, 38) then
+                                TriggerEvent("qb-burgershot:Tray2")
+                            end
+                        elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                            DrawText3D(v.x, v.y, v.z, "Tray")
+                        end  
+                    end
+                end
+            end
+
+            for k, v in pairs(Config.Locations["tray3"]) do
+                if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                    if onDuty then
+                        if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                            DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Tray")
+                            if IsControlJustReleased(0, 38) then
+                                TriggerEvent("qb-burgershot:Tray3")
+                            end
+                        elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                            DrawText3D(v.x, v.y, v.z, "Tray")
+                        end  
+                    end
+                end
+            end
+
+            for k, v in pairs(Config.Locations["tray4"]) do
+                if #(pos - vector3(v.x, v.y, v.z)) < 4.5 then
+                    if onDuty then
+                        if #(pos - vector3(v.x, v.y, v.z)) < 1.5 then
+                            DrawText3D(v.x, v.y, v.z, "~g~E~w~ -  Tray")
+                            if IsControlJustReleased(0, 38) then
+                                TriggerEvent("qb-burgershot:Tray4")
+                            end
+                        elseif #(pos - vector3(v.x, v.y, v.z)) < 2.5 then
+                            DrawText3D(v.x, v.y, v.z, "Tray")
+                        end  
+                    end
+                end
+            end
+        end
+    end
+end)
+
+-- NH - Context --
+
+RegisterNetEvent('nh-context:Burgers', function(data)
+    TriggerEvent('nh-context:sendMenu', {
+        {
+            id = 0,
+            header = "| Available Burgers |",
+            txt = "",
+        },
+        {
+            id = 1,
+            header = "• Moneyshot Burger",
+            txt = "Bun , Cooked Patty , Tomato , Lettuce",
+            params = {
+                event = "qb-burgershot:MoneyShot"
+            }
+        },
+        {
+            id = 2,
+            header = "• Meat Free Burger",
+            txt = "Bun , Tomato , Lettuce",
+            params = {
+                event = "qb-burgershot:MeatFree"
+            }
+        },
+        {
+            id = 3,
+            header = "• Bleeder Burger",
+            txt = "Bun , Cooked Patty , Tomato , Lettuce",
+            params = {
+                event = "qb-burgershot:BleederBurger"
+            }
+        },
+        {
+            id = 4,
+            header = "• The Heart Stopper",
+            txt = "Bun , Cooked Patty , Tomato , Lettuce",
+            params = {
+                event = "qb-burgershot:HeartStopper"
+            }
+        },
+        {
+            id = 5,
+            header = "• Torpedo Roll",
+            txt = "Bun , Cooked Meat",
+            params = {
+                event = "qb-burgershot:Torpedo"
+            }
+        },
+        {
+            id = 6,
+            header = "• Murder Meal",
+            txt = "The Heart Stopper, Fries and SoftDrink",
+            params = {
+                event = "qb-burgershot:CreateMurderMeal"
+            }
+        },
+        {
+            id = 7,
+            header = "Close (ESC)",
+            txt = "",
+        },
+    })
+end)
+
+
+RegisterNetEvent('nh-context:OrderMenu', function(data)
+    TriggerEvent('nh-context:sendMenu', {
+        {
+            id = 0,
+            header = "| Fridge |",
+            txt = "",
+        },
+        {
+            id = 1,
+            header = "• Order Items",
+            txt = "Order New Ingredients!",
+            params = {
+                event = "qb-burgershot:shop"
+            }
+        },
+        {
+            id = 2,
+            header = "• Open Fridge",
+            txt = "See what you have in storage",
+            params = {
+                event = "qb-burgershot:Storage2"
+            }
+        },
+        {
+            id = 3,
+            header = "Close (ESC)",
+            txt = "",
+        },
+    })
+end)
+
+RegisterNetEvent('nh-context:DrinkMenu', function(data)
+    TriggerEvent('nh-context:sendMenu', {
+        {
+            id = 0,
+            header = "| Drink Menu |",
+            txt = "",
+        },
+        {
+            id = 1,
+            header = "• Soft Drink",
+            txt = "Soda Syrup",
+            params = {
+                event = "qb-burgershot:SoftDrink"
+            }
+        },
+        {
+            id = 2,
+            header = "• Milkshake",
+            txt = "Milkshake Formula",
+            params = {
+                event = "qb-burgershot:mShake"
+            }
+        },
+        {
+            id = 3,
+            header = "Close (ESC)",
+            txt = "",
+        },
+    })
+end)
+
+
+-- Till Stuff --
+RegisterNetEvent("qb-burgershot:bill")
+AddEventHandler("qb-burgershot:bill", function()
+    local bill = exports["nh-keyboard"]:KeyboardInput({
+        header = "Create Receipt",
+        rows = {
+            {
+                id = 0,
+                txt = "Server ID"
+            },
+            {
+                id = 1,
+                txt = "Amount"
+            }
+        }
+    })
+    if bill ~= nil then
+        if bill[1].input == nil or bill[2].input == nil then 
+            return 
+        end
+        TriggerServerEvent("qb-burgershot:bill:player", bill[1].input, bill[2].input)
+    end
+end)
+
