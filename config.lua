522f7565438b59e970ee495cedc5e30703cbbfa1
Zach <67765614+DrBlackBeard095@users.noreply.github.com>
2021-09-10 08:47:51 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ Config.GaragePedlocation = {
     {cords = vector3(-1178.588, -891.9196, 13.799622 -1), h = 302.58825}  -- Garage NPC Spawn Location.
 }
 
+--if u are not using qb-target uncomment this.
 
 -- Config.Locations = {
 --     ["duty"] = {
