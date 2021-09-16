0e881ee2a84a83aa69614376b9f06e28d82fe3f2
Zach <67765614+DrBlackBeard095@users.noreply.github.com>
2021-09-16 17:31:55 +0200

    Added Config Option for ox sql

--- server/main.lua
+++ server/main.lua
@@ -7,12 +7,21 @@ AddEventHandler("qb-burgershot:bill:player", function(playerId, amount)
             if billed ~= nil then
                 if biller.PlayerData.citizenid ~= billed.PlayerData.citizenid then
                     if amount and amount > 0 then
+                       if Config.SQL == 'oxmysql' then
                         exports.oxmysql:insert('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (:citizenid, :amount, :society, :sender)', {
                             ['citizenid'] = billed.PlayerData.citizenid,
                             ['amount'] = amount,
                             ['society'] = biller.PlayerData.job.name,
                             ['sender'] = biller.PlayerData.charinfo.firstname
                         })
+                        else
+                          exports.ghmattimysql:execute('INSERT INTO phone_invoices (citizenid, amount, society, sender) VALUES (@citizenid, @amount, @society, @sender)', {
+                            ['@citizenid'] = billed.PlayerData.citizenid,
+                            ['@amount'] = amount,
+                            ['@society'] = biller.PlayerData.job.name,
+                            ['@sender'] = biller.PlayerData.charinfo.firstname
+                           })
+                       end
                         TriggerClientEvent('qb-phone:RefreshPhone', billed.PlayerData.source)
                         TriggerClientEvent('QBCore:Notify', source, 'Invoice Successfully Sent', 'success')
                         TriggerClientEvent('QBCore:Notify', billed.PlayerData.source, 'New Invoice Received')
