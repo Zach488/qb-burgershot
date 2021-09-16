d6c00ff6a1f18cf9da492d3fd72729fa94ece3ef
Zach <67765614+DrBlackBeard095@users.noreply.github.com>
2021-09-16 17:32:29 +0200

    Added Config Option for ox sql

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ Config.GaragePedLocations = {
     {coords = vector4(-1178.588, -891.9196, 13.799622, 302.58825)},
 }
 
+Config.SQL = 'oxmysql' --- oxmysql or ghmattisql
+
 --if u are not using qb-target uncomment this.
 
 -- Config.Locations = {
