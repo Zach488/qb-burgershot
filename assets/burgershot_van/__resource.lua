d6bd5b55a8d49be3487f445e0c98cf7f69844526
Zach <67765614+Zach488@users.noreply.github.com>
2023-03-12 12:57:06 +0200

    Update __resource.lua

--- assets/burgershot_van/__resource.lua
+++ assets/burgershot_van/__resource.lua
@@ -11,6 +11,3 @@ data_file 'HANDLING_FILE' 'handling.meta'
 data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-
-
-client_script 'vehicle_names.lua'
